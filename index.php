354c611e6b8743db7fd2f2052afa9ecf216fd86f
Adhityarismawan <adhityarismawan.m@gmail.com>
2016-06-04 12:43:24 +0700

    test 3

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="css/sb-admin.css" rel="stylesheet">
+
+<!-- Morris Charts CSS -->
+<link href="css/plugins/morris.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="font-awesome-4.1.0/css/font-awesome.min.css"
+	rel="stylesheet" type="text/css">
+
+<link
+	href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700'
+	rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="css/reset.css">
+<!-- CSS reset -->
+<link rel="stylesheet" href="css/style.css">
+<!-- Resource style -->
+<script src="js/modernizr.js"></script>
+<!-- Modernizr -->
+<script src="js/jquery-1.11.0.js"></script>
+<script src="js/main.js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script src="js/gmaps.js"></script>
+<!-- Resource jQuery -->
+<title>The Wedding Ria & Iqbal</title>
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.3";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	<div class="row">
+		<div class="col-lg-11 text-center" style="float: none; margin: 0 auto;" >
+				<img src="images/header.png" width="90%" style="margin-bottom: 15px;">
+					<div class="col-lg-10 text-center" style="float: none; margin: 0 auto;" >
+						<div class="panel panel-default">
+						<img src="images/header.jpg" >
+						<hr>
+						<h4>
+							"Dan diantara tanda-tanda kekuasaanNya ialah <br>
+							Dia menciptakan untukmu isteri-isteri dari jenismu sendiri, <br>
+							supaya kamu cenderung dan merasa tenteram kepadanya, <br>
+							dan dijadikanNya diantaramu rasa kasih dan sayang. <br>
+							Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berpikir."<br>
+							<i>[QS. Ar. Ruum (30):21]</i>
+						</h4>
+						<br> <br>
+						<h2 style="color: #69ABB7">THE WEDDING</h2>
+						<section id="cd-timeline" class="cd-container">
+							<div class="cd-timeline-block">
+								<div class="cd-timeline-img cd-picture">
+									<img src="img/cd-icon-picture.svg" alt="Picture">
+								</div>
+								<!-- cd-timeline-img -->
+	
+								<div class="cd-timeline-content">
+									<h2>Who ?</h2>
+									<br>
+									<img src="images/who.jpg" class="img-circle"
+										alt="Ria & Iqbal" width="304" height="236">
+									<h4 class="cd-date" style="font-style: italic;">
+										Ria Ekasari - Ria
+										<br>&<br>
+										Moch. Iqbal Wicaksono - Iqbal
+									</h4>
+								</div>
+								<!-- cd-timeline-content -->
+							</div>
+							<!-- cd-timeline-block -->
+	
+							<div class="cd-timeline-block">
+								<div class="cd-timeline-img cd-movie">
+									<img src="img/cd-icon-movie.svg" alt="Movie">
+								</div>
+								<!-- cd-timeline-img -->
+	
+								<div class="cd-timeline-content">
+									<h2>When ?</h2>
+									<br> 
+									<img src="images/when.jpg"
+										class="img-thumbnail" alt="Ria & Iqbal" width="304" height="236">
+									<h4 class="cd-date" style="font-style: italic;text-align: center;">
+										Minggu, 24 Juni 2012<br><br>
+										Akad Nikah - Pukul 09.00<br><br>
+										Resepsi - Pukul 11.00 - Selesai
+									</h4>
+								</div>
+								<!-- cd-timeline-content -->
+							</div>
+							<!-- cd-timeline-block -->
+	
+							<div class="cd-timeline-block">
+								<div class="cd-timeline-img cd-location">
+									<img src="img/cd-icon-location.svg" alt="Location">
+								</div>
+								<!-- cd-timeline-img -->
+	
+								<div class="cd-timeline-content">
+									<h2>Where ?</h2>
+									<br> <img src="images/where.jpg" class="img-rounded"
+										alt="Ria & Iqbal" width="304" height="236"> 
+									<div id="map" class="cd-date">
+									</div>
+								</div>
+								<!-- cd-timeline-content -->
+							</div>
+							<!-- cd-timeline-block -->
+	
+							<div class="cd-timeline-block">
+								<div class="cd-timeline-img cd-picture">
+									<img src="img/cd-icon-picture.svg" alt="Location">
+								</div>
+								<!-- cd-timeline-img -->
+	
+								<div class="cd-timeline-content">
+									<h2>About Us</h2>
+									<br> <img src="images/about.jpg" class="img-circle"
+										alt="First Met" width="200" height="350">
+									<span
+										class="cd-date">Ria & Iqbal</span>
+								</div>
+								<!-- cd-timeline-content -->
+							</div>
+							<!-- cd-timeline-block -->
+	
+						</section>
+						<!-- cd-timeline -->
+						<div style="float: none; margin: 0 auto;"
+							class="col-lg-3 text-center">
+							<i class="fa fa-5x fa-clock-o"></i>
+							<h3>"Every moment without you, is a moment of time lost."</h3>
+							<br> <br>
+							<h1>- Ria & Iqbal, 24 Juni 2012 -</h1>
+						</div>
+						<br>
+			</div>
+		</div>
+		<img src="images/divider.png" width="90%">
+		<div class="col-lg-10 text-center" style="float: none; margin: 0 auto;" >
+						<div class="panel panel-default">
+			<div class="fb-comments" data-href="http://riaiqbal.bagimoment.com/" data-width="100%" data-numposts="10" data-colorscheme="light"></div>				
+			<div class="fb-like" data-href="http://riaiqbal.bagimoment.com/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+			<br>
+			<br>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="row" style="margin-top: 20px;">
+		<div class="col-lg-11 text-center" style="float: none; margin: 0 auto;" >
+			<div style="float: none; margin: 0 auto;"
+				class="col-lg-10 text-center">
+				<div class="panel panel-default">
+					<div class="panel-body">
+					&copy; All Rights Reserved - <a href="http://bagimoment.com">Bagimoment.com</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<audio style="display: none;" controls autoplay>
+		<source src="audio/backsound.mp3" type="audio/mpeg">
+	</audio>
+	<a href="#0" class="cd-top">Top</a>
+</body>
+</html>
